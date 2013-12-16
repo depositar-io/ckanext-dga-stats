b7b3043b725e370ecb8a8c4e6cc569811fb42280
root <root@ip-172-31-18-206.ap-southeast-2.compute.internal>
2013-12-16 03:34:38 +0000

    additional overview stats

--- ckanext/dga_stats/stats.py
+++ ckanext/dga_stats/stats.py
@@ -123,10 +123,12 @@ class Stats(object):
     def summary_stats(cls):
        connection = model.Session.connection()
 
-#				select 'Total Archived Datasets', count(*) from package where (state='active' or state='draft' or state='draft-complete') and private = 't' union \
        res = connection.execute("SELECT 'Total Organisations', count(*) from \"group\" where type = 'organization' and state = 'active' union \
 				select 'Total Datasets', count(*) from package where (state='active' or state='draft' or state='draft-complete') and private = 'f' union \
-				select 'Total Data Files/Resources', count(*) from resource where state='active'").fetchall();
+				select 'Total Archived Datasets', count(*) from package where (state='active' or state='draft' or state='draft-complete') and private = 't' union \
+				select 'Total Data Files/Resources', count(*) from resource where state='active' union \
+				select 'Total Machine Readable/Data API Resources', count(*) from resource where state='active' and webstore_url = 'active'\
+				").fetchall();
        return res
 
 
